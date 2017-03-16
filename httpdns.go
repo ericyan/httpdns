a947caf3733d0f5aae1edc43de6c45b62cf235cd
Eric Yan <long@ericyan.me>
2017-03-16 23:53:46 +0800

    Allow to specify interface and port

--- httpdns.go
+++ httpdns.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	"github.com/miekg/dns"
 )
 
 func main() {
+	bind := flag.String("bind", "127.0.0.1", "interface to bind")
+	port := flag.Int("port", 8653, "port to run on")
+	flag.Parse()
+
 	dns.Handle(".", handler{dnspod})
 	dns.HandleFunc("httpdns.", instrumentation)
 
 	go func() {
-		srv := &dns.Server{Addr: ":8653", Net: "udp"}
+		srv := &dns.Server{Addr: *bind + ":" + strconv.Itoa(*port), Net: "udp"}
 		if err := srv.ListenAndServe(); err != nil {
 			log.Fatalf("Failed to set udp listener %s\n", err.Error())
 		}
